b7c6513fd0b08181299007ee2eeee5dd0d8de403
Navaneeth <navaneeth.g@licious.com>
2022-12-18 19:21:37 +0530

    LC-10 : Regex Match

---
+++ dec22/10.regular-expression-matching.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=10 lang=java
+ *
+ * [10] Regular Expression Matching
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isMatch(String text, String pattern) {
+        if (pattern.isEmpty()) return text.isEmpty();
+        boolean first_match = (!text.isEmpty() &&
+                               (pattern.charAt(0) == text.charAt(0) || pattern.charAt(0) == '.'));
+
+        if (pattern.length() >= 2 && pattern.charAt(1) == '*'){
+            return (isMatch(text, pattern.substring(2)) ||
+                    (first_match && isMatch(text.substring(1), pattern)));
+        } else {
+            return first_match && isMatch(text.substring(1), pattern.substring(1));
+        }
+    }
+}
+// @lc code=end
+
