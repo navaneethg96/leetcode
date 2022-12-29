1154fc08091715a2761d1eabe44bc62f0c104758
Navaneeth <navaneeth.g@licious.com>
2022-12-29 19:55:05 +0530

    LC-101 : symmetric tree

---
+++ dec22/101.symmetric-tree.java
@@ -0,0 +1,45 @@
+
+/*
+ * @lc app=leetcode id=101 lang=java
+ *
+ * [101] Symmetric Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return checkSymmetry(root.left, root.right);
+    }
+
+    private boolean checkSymmetry(TreeNode leftTree, TreeNode rightTree) {
+        if (leftTree == null && rightTree == null) {
+            return true;
+        }
+        if (leftTree == null || rightTree == null) {
+            return false;
+        }
+        return leftTree.val == rightTree.val
+            && checkSymmetry(leftTree.left, rightTree.right)
+            && checkSymmetry(leftTree.right, rightTree.left);
+    }
+}
+// @lc code=end
+
